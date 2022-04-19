c30b16a0d4e8af1cc0d8b167253b972ce61b242c
karunakarbhogyari <karunakar1008@gmail.com>
2022-04-19 13:46:09 +0530

    kv web app

---
+++ KeyvaultDemoWebApp/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Azure.KeyVault;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KeyvaultDemoWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            var kv = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(Utils.GetToken));
+            var sec1 = await kv.GetSecretAsync(ConfigurationManager.AppSettings["Secreturi"]);
+            var key1 = await kv.GetKeyAsync(ConfigurationManager.AppSettings["MyFirstkeyUri"]);
+            Console.WriteLine(sec1.Value);
+            Console.WriteLine(key1);
+            ViewBag.KeyvaultSecretValue = sec1.Value;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ KeyvaultDemoWebApp/Utils.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace KeyvaultDemoWebApp
+{
+    public class Utils
+    {
+        static string appId = ConfigurationManager.AppSettings["ClientId"];
+        static string appSecret = ConfigurationManager.AppSettings["ClientSecret"];
+
+
+        public static async Task<string> GetToken(string authority, string resource, string scope)
+        {
+            var authContext = new AuthenticationContext(authority);
+            ClientCredential clientCred = new ClientCredential(appId, appSecret);
+            AuthenticationResult result = await authContext.AcquireTokenAsync(resource, clientCred);
+            if (result == null)
+                throw new InvalidOperationException("Failed to obtain the JWT token");
+            return result.AccessToken;
+        }
+    }
+}
