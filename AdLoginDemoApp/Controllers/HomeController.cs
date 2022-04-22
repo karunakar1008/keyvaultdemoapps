025ec14d32ebfec2f1f7e8191c629cad72a32c39
karunakarbhogyari <karunakar1008@gmail.com>
2022-04-22 10:11:04 +0530

    Single tenant AD login.

---
+++ AdLoginDemoApp/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using AdLoginDemoApp.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AdLoginDemoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult About()
+        {
+            ViewBag.Name = User.Claims.FirstOrDefault(claim => claim.Type == "name")?.Value;
+            ViewBag.Username = User.Claims.FirstOrDefault(claim => claim.Type == "preferred_username")?.Value;
+            ViewBag.TenantId = User.Claims.FirstOrDefault(claim => claim.Type == "http://schemas.microsoft.com/identity/claims/tenantid")?.Value;
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
