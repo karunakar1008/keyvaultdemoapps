d9da3d2531bfc8a3ba18a998dff2ea6b841b55e7
karunakarbhogyari <karunakar1008@gmail.com>
2022-04-23 19:21:18 +0530

    Move code to a AD programming folder

--- ADALDemoApp/Program.cs
+++ ADALDemoApp/Program.cs
@@ -5,19 +5,19 @@ namespace ADALDemoApp
 {
     class Program
     {
-       
-            static string appId = "dedb6748-772b-4679-998c-3a39a77dec60";
-            static string secret = "~4V8Q~cwygDAyOZaXDfVWzDFHMzNnsRrFUT5ybgA";
-            static string tenantId = "0c44cc27-a1cd-4b20-9c98-607c791fc563";
-            static void Main(string[] args)
-            {
-                var context = new AuthenticationContext("https://login.windows.net/" + tenantId);
-                var credential = new ClientCredential(clientId: appId, clientSecret: secret);
-                AuthenticationResult result = context.AcquireTokenAsync(appId, credential).Result;
-                if (result == null)
-                    throw new InvalidOperationException("Failed to obtain the JWT token");
-                var token = result.AccessToken;
-            }
+        static string appId = "11100d01-4e5f-4862-9bfd-fe9afb489dd0";
+        static string secret = "vMn8Q~3yQlKfVixmUEKHFFgsYHnoiyH5cCYA5bH2";
+        static string tenantId = "0c44cc27-a1cd-4b20-9c98-607c791fc563";
+        static void Main(string[] args)
+        {
+            var context = new AuthenticationContext("https://login.windows.net/" + tenantId);
+            var credential = new ClientCredential(clientId: appId, clientSecret: secret);
+            AuthenticationResult result = context.AcquireTokenAsync(appId, credential).Result;
+            if (result == null)
+                throw new InvalidOperationException("Failed to obtain the JWT token");
+            var token = result.AccessToken;
+            Console.WriteLine(token);
+        }
 
     }
 }
